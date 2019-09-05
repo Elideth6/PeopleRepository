dcb52c1eadd215ae9bc724c36b733a37648ba91c
Centro de Desarrollo De Software <cds@univo.edu.sv>
2019-09-05 08:40:29 -0600

    Model classe Person Added

---
+++ People/People/People/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace People.Models
+{
+    [Table("TBL_people")]
+   public class Person
+    {
+        //Miembro: Properties
+        [PrimaryKey, AutoIncrement]
+
+        public int id { get; send; }
+
+        [MaxLength(50), Unique]
+
+        public string Name { get; set; }
+    }
+}
